f4bee1c08c95d0aa319ad4ddde8251c9bc3ac8bf
jahid36 <58757654+jahid36@users.noreply.github.com>
2020-01-08 20:17:00 -0500

    Add files via upload

---
+++ MyFriends.java
@@ -0,0 +1,53 @@
+package com.jahid;
+
+public class MyFriends {
+
+    public static void main(String [] args ){
+    System.out.println("My Team");
+    myFirstJavaMethod();
+    mySecondJavaMethod();
+    myThirdJavaMethod();
+    myFourthJavaMethod();
+    myFiveJavaMethod();
+    mySixJavaMethod();
+
+}
+public static void myFirstJavaMethod(){
+    int one =1;
+    String second ="Jahid";
+    int a=3;
+    int b=4;
+    int c=5;
+
+    System.out.println(one+a+b+c+"second");
+}
+public static void mySecondJavaMethod(){
+    int one =2;
+    String second = "jahid";
+    System.out.println(one+"jahid");
+}
+public static void myThirdJavaMethod(){
+    int one = 3;
+    String second = "Jakir Mama";
+    System.out.println(one+"Jakir Mama");
+}
+public static void myFourthJavaMethod(){
+    int one = 4;
+    String second = "Towhid Bhai";
+    System.out.println(one+"Towhid Bhai");
+
+}
+public static void myFiveJavaMethod(){
+    int one = 5;
+    String second = "Humayon Mama";
+    System.out.println(one+"Humayon Mama");
+}
+public static void mySixJavaMethod(){
+        int one=6;
+        String second="Mamun Mama";
+        System.out.println(one+"Mamun Mama");
+
+
+        }
+        }
+
