d5150bf3a26f16a092bd3106051534a992c9e6dc
jahid36 <58757654+jahid36@users.noreply.github.com>
2020-01-08 20:16:30 -0500

    Add files via upload

---
+++ UnaryOperator.java
@@ -0,0 +1,17 @@
+package com;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int myIntVal = 10;
+
+        //myIntVal ++;
+        myIntVal--;
+
+        boolean myBoolean = !true;
+
+
+        //System.out.println(myIntVal);
+        System.out.println(2 < 3);
+
+    }
+}
